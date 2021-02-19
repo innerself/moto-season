50202b8f896848b69591aa227c75bf39f3ea50cd
Sergey Petrin <spetrin@gmail.com>
2021-02-19 23:58:05 +0300

    Add basic graph

--- temperature/views.py
+++ temperature/views.py
@@ -1,5 +1,31 @@
 from django.http import HttpResponse
+from plotly.offline import plot
+from plotly import graph_objs
+
+from temperature.models import Temperature
 
 
 def main(request):
-    return HttpResponse()
+    temps = Temperature.objects.all()
+    season_start_temperature = 5
+
+    fig = graph_objs.Figure()
+    actual_temp_line = graph_objs.Scatter(
+        x=[t.date for t in temps],
+        y=[t.min for t in temps],
+        mode='lines',
+        name='Current/Forecast',
+        opacity=0.8,
+    )
+    season_start_temp_line = graph_objs.Scatter(
+        x=[t.date for t in temps],
+        y=[season_start_temperature for _ in range(len(temps))],
+        mode='lines',
+        name='Season start',
+        opacity=0.2,
+    )
+    fig.add_trace(actual_temp_line)
+    fig.add_trace(season_start_temp_line)
+    plt_div = plot(fig, output_type='div')
+
+    return HttpResponse(plt_div)
